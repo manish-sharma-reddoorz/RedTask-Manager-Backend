7f4939c74e94b16e599187e137c2bb210073ade6
manish-sharma-reddoorz <manish.sharma@reddoorz.com>
2024-03-07 00:16:15 +0530

    Completed CRUD task services

---
+++ src/services/task-service.js
@@ -0,0 +1,57 @@
+import { TaskRepository } from "../repository";
+
+class TaskService {
+    constructor() {
+        this.taskRepository = new TaskRepository();
+    }
+
+
+    async create(data) {
+
+        try {
+            const task = await this.taskRepository.create(data);
+            return task;
+        } catch (error) {
+            console.log("Something went wrong in task service");
+            throw error;
+        }
+    }
+
+    async destroy(id) {
+
+        try {
+            const task = await this.taskRepository.destroy(id);
+            return task;
+        } catch (error) {
+            console.log("Something went wrong in task service");
+            throw error;
+        }
+    }
+
+
+
+    async get(id) {
+
+        try {
+            const task = await this.taskRepository.get(id);
+            return task;
+        } catch (error) {
+            console.log("Something went wrong in task service");
+            throw error;
+        }
+    }
+
+    async update(taskId, data) {
+
+        try {
+            const task = await this.taskRepository.update(taskId, data);
+            return task;
+        } catch (error) {
+            console.log("Something went wrong in task service");
+            throw error;
+        }
+    }
+}
+
+
+module.exports = TaskService;
