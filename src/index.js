d49366686467cfd3d8f815a6b506440f204b71bb
vani <vani.sheoran@reddoorz.com>
2024-03-11 12:42:23 +0530

    created-user-schema

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const prepareAndStartServer = async () => {
     app.use('/api', apiRoutes);
     app.listen(PORT, async () => {
         console.log(`Server started at PORT: ${PORT}`);
-        await connect();
+        // await connect();
         console.log('Mongo db connected');
 
     })
