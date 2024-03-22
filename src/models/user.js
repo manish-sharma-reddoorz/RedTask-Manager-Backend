2d1b4de617fce8ada618ef49707d0c443fde9535
vani <vani.sheoran@reddoorz.com>
2024-03-22 10:34:40 +0530

    task-repo

--- src/models/user.js
+++ src/models/user.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        name: {
-            type:String,
-            required: true
-        },
+        
         email: {
             type: String , 
             required: true
@@ -17,6 +14,10 @@ const userSchema = new mongoose.Schema(
 
         password: {
             type: String, 
+            required: true
+        },
+        cpassword : {
+            type: String,
             required: true
         }
        
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const TaskController = require('../../controllers/task-controller');
 
 
-// for task
+
+//for user
+router.post('/register', async (req,res)=>{
+    console.log('got the form')
+    res.send('got it')
+})
+
+//for task
 router.post(
     '/tasks',
     TaskController.create
@@ -13,3 +20,58 @@ router.patch('/tasks/:id', TaskController.update);
 router.get('/tasks/:id',TaskController.get);
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { email, phone, password, confirmPassword} = req.body;
+
+// if(  !email || !phone || !password || !confirmPassword){
+//             return res.status(422).json({error: "All fields are mandatory"})
+// }
+
+// })
+ 
+// try {
+//     const response = await User.findOne({email:email});
+//     if(userExists){
+//         return res.status(422).json({error:"Email already exists"})
+//     }
+//     const newUser = new User({email, phone, password, confirmPassword})
+//     const userRegister = await newUser.save()
+
+//     if(userRegister){
+//         res.status(201).json({message: "user registered successfully"})
+
+//     }else{
+//         res.status(500).json({error:"Failed to register"})
+//     }
+
+
+
+
+// }
+// catch(err){
+//     console.log(error)
+// }
+
+// for task
